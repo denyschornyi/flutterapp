fa9e28e4710ed768dde969a6147ba1dbd081d674
Denys Chornyi <chornijj14@gmail.com>
2021-04-10 20:16:01 +0200

    Add a button

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -65,6 +65,7 @@ class Home extends StatelessWidget {
               ],
             ),
             PizzaImageWidget(),
+            OrderButton(),
           ],
         ),
       ),
@@ -72,13 +73,42 @@ class Home extends StatelessWidget {
   }
 }
 
-
-class PizzaImageWidget extends StatelessWidget{
+class PizzaImageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     AssetImage pizzaAsset = AssetImage('images/pizza.png');
-    Image image = Image(image: pizzaAsset, width: 400, height: 400,);
+    Image image = Image(
+      image: pizzaAsset,
+      width: 400,
+      height: 400,
+    );
     return Container(child: image);
   }
+}
 
+class OrderButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var button = Container(
+      margin: EdgeInsets.only(top: 50),
+      child: ElevatedButton(
+        child: Text('Order your pizza'),
+        style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all<Color>(Colors.lightGreen)),
+        onPressed: () {
+          order(context);
+        },
+      ),
+    );
+    return button;
+  }
+
+  void order(BuildContext context) {
+    var alert = AlertDialog(
+      title: Text("Order completed"),
+      content: Text("Thanks for your order"),
+    );
+    showDialog(context: context, builder: (BuildContext context) => alert);
+  }
 }
