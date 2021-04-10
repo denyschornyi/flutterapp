38b56856853c85a161a5378512a4588421adbfba
Denys Chornyi <chornijj14@gmail.com>
2021-04-10 20:43:13 +0200

    Use a stetful widget to insert a text inside

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,114 +1,38 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String name = '';
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        alignment: Alignment.center,
-        color: Colors.deepOrangeAccent,
-        padding: EdgeInsets.only(top: 30, left: 10),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello'),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(15),
         child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    "Margerita",
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                        fontSize: 30.0,
-                        decoration: TextDecoration.none,
-                        fontFamily: 'Oxygen',
-                        fontWeight: FontWeight.w300),
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    "Tomato, Mozzarela, Basil",
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        decoration: TextDecoration.none,
-                        fontFamily: 'Oxygen',
-                        fontWeight: FontWeight.w300),
-                  ),
-                ),
-              ],
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Please provide your name"
+              ),
+              onChanged: (String string) {
+                setState(() {
+                  name = string;
+                });
+              },
             ),
-            Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    "Marinara",
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                        fontSize: 30.0,
-                        decoration: TextDecoration.none,
-                        fontFamily: 'Oxygen',
-                        fontWeight: FontWeight.w300),
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    "Tomato, Garlic",
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        decoration: TextDecoration.none,
-                        fontFamily: 'Oxygen',
-                        fontWeight: FontWeight.w300),
-                  ),
-                ),
-              ],
-            ),
-            PizzaImageWidget(),
-            OrderButton(),
+            Text("Hello ${name}"),
           ],
         ),
       ),
     );
-  }
-}
-
-class PizzaImageWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    AssetImage pizzaAsset = AssetImage('images/pizza.png');
-    Image image = Image(
-      image: pizzaAsset,
-      width: 400,
-      height: 400,
-    );
-    return Container(child: image);
-  }
-}
-
-class OrderButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var button = Container(
-      margin: EdgeInsets.only(top: 50),
-      child: ElevatedButton(
-        child: Text('Order your pizza'),
-        style: ButtonStyle(
-            backgroundColor:
-                MaterialStateProperty.all<Color>(Colors.lightGreen)),
-        onPressed: () {
-          order(context);
-        },
-      ),
-    );
-    return button;
-  }
-
-  void order(BuildContext context) {
-    var alert = AlertDialog(
-      title: Text("Order completed"),
-      content: Text("Thanks for your order"),
-    );
-    showDialog(context: context, builder: (BuildContext context) => alert);
   }
 }
